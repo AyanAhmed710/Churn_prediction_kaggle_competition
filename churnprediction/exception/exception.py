c592621cc3f3efdb6783dbf91d1150c958ee562b
AyanAhmad710 <sheikhayanahmad710@gmail.com>
2026-03-19 05:32:18 +0500

    2 :Logging and Exception Handling Added

---
+++ churnprediction/exception/exception.py
@@ -0,0 +1,19 @@
+import sys
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail: sys):
+        self.error_message=error_message
+        _ ,_ , exc_tb=error_detail.exc_info()
+
+        self.file_name=exc_tb.tb_frame.f_code.co_filename
+        self.line_number=exc_tb.tb_lineno
+
+    def __str__(self):
+
+        return f"Error occurred in file: {self.file_name} at line number: {self.line_number} with error message: {self.error_message}"  
+    
+
+    
+
+    
+        
---
+++ churnprediction/logging/logger.py
@@ -0,0 +1,33 @@
+import os
+import logging
+from datetime import datetime   
+from logging.handlers import RotatingFileHandler
+
+
+LOG_DIR="logs" 
+
+def get_logger(name):
+
+    os.makedirs(LOG_DIR, exist_ok=True)
+
+    log_file_path = os.path.join(LOG_DIR, f"{name}_{datetime.now().strftime('%Y%m%d_%H%M%S')}.log")
+
+    logger=logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+
+    file_handler=RotatingFileHandler(log_file_path, maxBytes=5*1024*1024, backupCount=5)
+    file_handler.setLevel(logging.DEBUG)
+
+    console_handler=logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG)
+
+    formatter=logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(formatter)
+    console_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+
+    return logger
+
+
